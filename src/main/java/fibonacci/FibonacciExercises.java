42942b5c42023b391e5b80f8e7c1c8684d774854
anastasia <stasia_s@rambler.ru>
2018-11-11 16:51:19 +0300

    Fibonacci done

--- src/main/java/fibonacci/FibonacciExercises.java
+++ src/main/java/fibonacci/FibonacciExercises.java
@@ -1,11 +1,25 @@
 package fibonacci;
 
+import java.util.ArrayList;
+
 import io.reactivex.Observable;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class FibonacciExercises {
 
+	//TODO: Нужно что-то придумать с if'ами
 	public Observable<Integer> fibonacci(int n) {
-		return Observable.error(new NotImplementedException());
+		if (n == 1) {
+			return Observable.just(0);
+		} else if (n < 1) {
+			Observable.error(new NotImplementedException());
+		}
+		Integer[] is = new Integer[n];
+		is[0] = 0;
+		is[1] = 1;
+		for (int i = 2; i < n; i++) {
+			is[i] = is[i-1] + is[i-2];
+		}
+		return Observable.fromArray(is);
 	}
 }
